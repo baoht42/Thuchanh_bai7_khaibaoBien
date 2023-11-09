cedf8b368f1909895c9391783fc551250db90469
baoht42 <snorlax4293@gmail.com>
2023-11-09 11:34:23 +0700

    nop bai thuc hanh khai bao Bien

---
+++ bai2.js
@@ -0,0 +1,4 @@
+let rong = 10;
+let dai = 20;
+let dtich = rong * dai;
+document.write("dien tich la: "+ dtich);
---
+++ bai3_TH.js
@@ -0,0 +1,9 @@
+
+    let a = prompt('Nhap a: ');
+    let b = prompt('Nhap b: ');
+    let c = a % b;
+    if (c==0) {
+        alert('a la boi so cua b');
+    }else{
+        alert('a ko phai boi so cua b');
+    }
